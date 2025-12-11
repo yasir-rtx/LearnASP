57b80da0661d9085a64faf0ec407a374d56134d1
yasir <yasir112358@gmail.com>
2025-12-11 15:35:03 +0700

    Remove data annotation from book model for best practice

--- Book.cs
+++ Book.cs
@@ -4,29 +4,16 @@ namespace Obscura.Models
 {
     public class Book
     {
-        [Key]
-        public int Id { get; set; }     
-        
-        [Required, MaxLength(200)]
+        public int Id { get; set; }
         public string Title { get; set; } = String.Empty;
-
-        [Required, MaxLength(100)]
         public string Author { get; set; } = String.Empty;
-
-        [MaxLength(2000)]
         public string? Description { get; set; }
-
-        [Required, MaxLength(100)]
         public string Publisher { get; set; } = String.Empty;
-
         public decimal Price { get; set; } = 0.0M;
-
         public DateTime CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
-
         public int CreatedBy { get; set; }
         public int UpdatedBy { get; set; }
-
         public bool IsDeleted { get; set; } = false;
         public bool IsAvailable { get; set; } = true;
         public bool IsForSale { get; set; } = true;
