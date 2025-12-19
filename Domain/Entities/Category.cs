002adca22979745adee166c96c9da6631977d06f
yashiru <yasir112358@gmail.com>
2025-12-19 10:15:19 +0700

    ADD Category Entity

---
+++ Domain/Entities/Category.cs
@@ -0,0 +1,15 @@
+namespace LearnASP.Domain.Entities
+{
+    public class Category
+    {
+        public int Id {get; set;}
+        public string Name {get; set;} = String.Empty;
+        public string Slug {get; set;} = String.Empty;
+        public string Description {get; set;} = String.Empty;
+        public bool IsActive {get; set;}
+        public int CreatedBy {get; set;}
+        public int UpdatedBy {get; set;}
+        public DateTime CreatedAt {get; set;}
+        public DateTime UpdatedAt {get; set;}
+    }
+}
